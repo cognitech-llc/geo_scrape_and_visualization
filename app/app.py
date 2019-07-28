2304149593bd668bbff8904dc69d6d16e4fc5d72
Finian O'Neill <finian.d.oneill@gmail.com>
2019-07-28 10:42:08 -0700

    Add in Heroku initialization files.

--- app/app.py
+++ app/app.py
@@ -33,4 +33,6 @@ def starbucks():
     return jsonify(json_list)
   
 if __name__ == "__main__":
-  app.run()
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
